1ef2a830fe89762cd1bf8e299e36dc6308f7b9cf
HighnessAtharva <68660002+HighnessAtharva@users.noreply.github.com>
2020-12-29 12:16:11 +0530

    Added ListStats mini-project

---
+++ ListStatistics/main.py
@@ -0,0 +1,16 @@
+import StatsFunctions
+lst = [6, 6, 10, 15, 9, 8, 17, 5]
+Stats = StatsFunctions.Stats
+
+print("Average\t", Stats.average(lst))
+print("MD\t", Stats.meanDeviation(lst))
+print("Count 7\t", Stats.count(lst, 7))
+print("Length\t", Stats.length(lst))
+print("Max\t", Stats.max(lst))
+print("Min\t", Stats.min(lst))
+print("Range\t", Stats.range(lst))
+print("Sum\t", Stats.sum(lst))
+print("Sort\t", Stats.sort(lst))
+print("Variance", Stats.variance(lst))
+print("SD\t", Stats.standardDeviation(lst))
+print("Median\t", Stats.median(lst))
