304e52ed7735f2e4382dfa584a747786838f6fa7
HighnessAtharva <68660002+HighnessAtharva@users.noreply.github.com>
2021-01-20 16:39:37 +0530

    Code Cleaned

--- Web-Scraper/WebScraper.py
+++ Web-Scraper/WebScraper.py
@@ -1,13 +1,15 @@
-#import requests
 import sys
 import re
 import urllib.request
 from bs4 import BeautifulSoup
-#from urllib import urlopen
-import re
 from urllib.request import Request, urlopen
+'''NOTE:Arbitary Links for Reference (Only works for 1000 MCQs homepage)
+https://www.sanfoundry.com/1000-python-questions-answers/
 
-req = Request('https://www.sanfoundry.com/operating-system-questions-answers/', headers={'User-Agent': 'Mozilla/5.0'})
+https://www.sanfoundry.com/1000-digital-image-processing-questions-answers/
+'''
+req = Request('https://www.sanfoundry.com/1000-fluid-mechanics-questions-answers/',
+              headers={'User-Agent': 'Mozilla/5.0'})
 webpage = urllib.request.urlopen(req).read()
 html_page = urlopen(req)
 
@@ -18,11 +20,11 @@ for link in soup.findAll('a'):
     x = re.search("-answers-", str(link))
     if x:
         links.append(link.get('href'))
-    
+
 for link in links:
     with open("output.txt", "a") as f:
-         
+
         print(link, file=f)
         print('\n', file=f)
 
-'''NOTE: The Text file will be saved @ Users>Your_Username>output.txt'''
+'''NOTE: The Text file will be saved @ TermainlPath>output.txt so change you can cd from terminal and execute script there'''
