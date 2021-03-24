9aea23de027eda6022117372c97457723e470bab
HighnessAtharva <68660002+HighnessAtharva@users.noreply.github.com>
2021-03-24 22:23:49 +0530

    Counter Programer Added

---
+++ Countdown/main.py
@@ -0,0 +1,22 @@
+import time
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+    print("Time Out")
+
+
+minsOrSecs = input(
+    "Do you want to set the countdown by minutes[M] or by seconds[S]?")
+if minsOrSecs == 'M':
+    t = int(input("Enter the number of minutes for countdown: "))
+    t = 60*t
+    t = countdown(t)
+
+if minsOrSecs == 'S':
+    t = int(input("Enter the number of seconds for countdown."))
+    t = countdown(t)
